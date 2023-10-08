10e8e9d97a4ce85eaa512ebfb44c9ca86cbbb411
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2023-10-08 12:56:24 +0600

    again initiallize

---
+++ src/pages/components/navbar/NavBar.tsx
@@ -0,0 +1,10 @@
+function NavBar() {
+  return (
+    <div className="flex flex-row align-middle justify-between">
+      <div className="">logo || name</div>
+      <div className="">search || write ||| else</div>
+    </div>
+  );
+}
+
+export default NavBar;
