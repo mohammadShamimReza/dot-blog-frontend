2c97f17f474a3160f96b9b30bdc31a8189617a13
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2023-11-06 18:19:28 +0600

    add fixed navbar

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -13,7 +13,7 @@ function Layout({ children }: { children: ReactElement }) {
       <Providers>
         <div className="">
           <NavBar />
-          <div className="min-h-screen flex justify-center m-4">
+          <div className="min-h-screen flex justify-center ">
             <div className="w-full max-w-5xl p-4 border border-b-0 border-t-0  border-r-gray-200 border-l-gray-200 dark:border-l-gray-600 dark:border-r-gray-600 ">
               {children}
             </div>
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,12 +1,35 @@
 import Link from "next/link";
+import { useEffect } from "react";
 import { SiGooglemessages } from "react-icons/si";
 import { TfiWrite } from "react-icons/tfi";
 import ThemeToggle from "./ThemeToggle";
 import User from "./User";
 
 function NavBar() {
+  const handleScroll = () => {
+    const navbar = document.getElementById("navbar");
+    if (navbar) {
+      // Check if navbar is not null
+      if (window.scrollY > 0) {
+        navbar.classList.add("fixed", "bg-gray-200", "dark:bg-gray-700"); // Add your desired background color class
+      } else {
+        navbar.classList.remove("fixed", "bg-gray-200", "dark:bg-gray-700"); // Remove the background color class
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="p-4 border border-t-0 border-l-0 border-r-0 border-b-gray-200 dark:border-b-gray-600">
+    <nav
+      id="navbar"
+      className="p-4 border border-t-0 border-l-0 border-r-0 border-b-gray-200 dark:border-b-gray-600 z-10 w-full"
+    >
       <div className="flex items-center justify-between">
         <Link href="/">
           <div className="p-2">MS-Blog</div>
