eac63b3a56a88baa89b1fcee5a5ac2c15af74f0d
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2023-11-09 19:02:19 +0600

    add url

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,6 +1,6 @@
 export const getBaseUrl = (): string => {
   return (
     process.env.NEXT_PUBLIC_API_BASE_URL ||
-    "https://dot-blog-backend.vercel.app//api/v1"
+    "https://dot-blog-backend.vercel.app/api/v1"
   );
 };
